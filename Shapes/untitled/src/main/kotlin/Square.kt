538c2ab4271266b1e479398e0cbabef33d00eda1
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-10-10 23:13:41 +0800

    Fix resize() of Square

--- Shapes/untitled/src/main/kotlin/Square.kt
+++ Shapes/untitled/src/main/kotlin/Square.kt
@@ -1,5 +1,10 @@
 class Square(var x_: Int, var y_: Int, var a: Int) : Rect(x_, y_, a, a) {
     // Let square be a rect
 
+    override fun resize(zoom: Int) {
+        super.resize(zoom)
+        a *= zoom
+    }
+
     override fun toString() = "Square(${x}, ${y}, ${a})"
 }
